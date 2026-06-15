95bdfce6d0897c2048b958e71b41e670e5fab030
Yaroslav-Hrytsan <yaroslav.hrytsan@gmail.com>
2026-06-16 00:25:08 +0300

    fix base.html

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("register/", UserRegisterView.as_view(), name= 'register'),
     path("login/", UserLoginView.as_view(), name= 'login'),
     path("profile/", UserProfileView.as_view(), name= 'profile'),
-    path("users/", UserListView.as_view(), name= 'list'),
+    path("users/", UserListView.as_view(), name= 'user_list'),
 ]
