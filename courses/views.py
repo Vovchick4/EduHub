be968b55a6d4f898b324b8aa5c0b6c29cbe9259f
Yaroslav-Hrytsan <yaroslav.hrytsan@gmail.com>
2026-06-22 12:55:29 +0300

    feature/KAN-6 add courses-list

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.core.paginator import Paginator
+from courses.models import Course
 
-# Create your views here.
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/course_list.html'
+    context_object_name = 'courses'
+    paginate_by = 6
+    
