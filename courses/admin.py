6fdd0e830107e32bd6d8c49b982f9a2fcb7e767b
General202 <user.den.ua@gmail.com>
2026-06-22 18:07:33 +0300

    feat(admin): customize django admin for courses and lessons KAN-5

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Course
+from lessons.models import Lesson
 
 # Register your models here.
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 1  
+    fields = ('order', 'title', 'content')  
+    ordering = ('order',)  
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'created_at', 'updated_at')
+    search_fields = ('name', 'description', 'author__email') 
+    list_filter = ('created_at', 'author')
+    
+    inlines = [LessonInline]
--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Lesson
 
-# Register your models here.
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'order', 'created_at')
+    search_fields = ('title', 'content', 'course__name')
+    list_filter = ('course', 'created_at')
+    ordering = ('course', 'order')
